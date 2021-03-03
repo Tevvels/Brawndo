02e390a28c39e157c62b3c2e618256b04ccefc1f
Wendy Tekverk <wtekverk@gmail.com>
2021-03-03 13:10:02 -0700

    adding initial HB

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,10 @@
-const router = require("express").Router()
-const db = require('./models');
+const db = require('..models/');
 
-
-router.get("/brawndo", (req,res) => {
- db.Brawndo.findAll({}).then(data => {
-     console.log(data)
-     res.json(data)
- })
-})
-
-module.exports = router
+module.exports = (app) => {
+	app.get('/brawndo', (req, res) => {
+		db.Brawndo.findAll({}).then((data) => {
+			console.log(data);
+			res.json(data);
+		});
+	});
+};
