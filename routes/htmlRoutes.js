732d0fe25b1133ae46bc265560bbf732103e7fcc
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-03 13:32:43 -0700

    html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,4 +9,8 @@ module.exports = (app) => {
 	app.get('/', (req, res) =>
 		res.sendFile(path.join(__dirname, '../public/index.html'))
 	);
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.join(__dirname, '../public/index.html'))
+	);
 };
