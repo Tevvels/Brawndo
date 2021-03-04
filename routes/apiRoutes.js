85063c82d1a9f79c3301e3ed18f5e9119e47fec9
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-04 13:54:06 -0700

    work on routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,27 +4,31 @@ const express = require('express');
 
 module.exports = (app) => {
 	app.get('/', function (req, res) {
-		// * If plants exist in database, redirect to /garden
-		res.redirect('/garden');
-		// * Else redirect to create garden page
-		// res.redirect("/createGarden");
+		// If plants exist in database, redirect to /garden. If no plants, redirect to /garden/create
+		db.Plant.findAll().then((data) => {
+			// * below functionality not currently working...need to debug
+			// console.log(data);
+			if (data) {
+				res.redirect('/garden');
+			} else {
+				res.redirect('/garden/create');
+			}
+		});
 	});
 
-	// get route, edited to match sequelize
+	// get route that displays all plants
 	app.get('/garden', function (req, res) {
-		// replace old function with sequelize function
-		db.Plant.findAll()
-			// use promise method to pass the burgers...
-			.then(function (data) {
-				console.log(data);
-				// into the main index, updating the page
-				var hbsObject = { garden: data };
-				return res.render('index', hbsObject);
-			});
+		db.Plant.findAll().then(function (data) {
+			console.log(data);
+			var hbsObject = { garden: data };
+			return res.render('index', hbsObject);
+		});
 	});
 
-	// post route that inserts a new plant
+	// app.get('/garden/create', function (req, res) {});
+
+	// post route that inserts a new plant into Plants in brawndo_db
 	app.post('/garden/create', (req, res) => {
-		db.Plant.create(req.body).then((data) => res.redirect('/'));
+		db.Plant.create(req.body).then(res.redirect('/garden'));
 	});
 };
