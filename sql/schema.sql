db7e385189efecbd33e73d621d2dd1e39c2da46d
domihorinka <domihorinka@gmail.com>
2021-03-04 10:30:54 -0700

    created table in schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,2 +1,14 @@
 DROP DATABASE IF EXISTS brawndo_db;
 CREATE DATABASE brawndo_db;
+USE brawndo_db;
+
+CREATE TABLE plants(
+id INT NOT NULL AUTO_INCREMENT,
+plant_name VARCHAR(100) NOT NULL,
+outdoor BOOLEAN NOT NULL,
+indoor BOOLEAN NOT NULL,
+water_frequency INT NOT NULL,
+water_times INT NOT NULL
+);
+
+
