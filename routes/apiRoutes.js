368a62845ceb9779d68c5f071159557b35e78c50
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-04 13:13:23 -0700

    work on routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,25 +2,27 @@ const db = require('../models');
 const express = require('express');
 // var router = express.Router();
 
-app.get('/', function (req, res) {
-	// * If plants exist in database, redirect to /garden
-	// res.redirect("/garden");
-	// * Else redirect to create garden page
-	// res.redirect("/createGarden");
-});
+module.exports = (app) => {
+	app.get('/', function (req, res) {
+		// * If plants exist in database, redirect to /garden
+		// res.redirect("/garden");
+		// * Else redirect to create garden page
+		// res.redirect("/createGarden");
+	});
 
-// get route, edited to match sequelize
-app.get('/garden', function (req, res) {
-	// replace old function with sequelize function
-	db.Plant.findAll()
-		// use promise method to pass the burgers...
-		.then(function (data) {
-			console.log(data);
-			// into the main index, updating the page
-			var hbsObject = { garden: data };
-			return res.render('index', hbsObject);
-		});
-});
+	// get route, edited to match sequelize
+	app.get('/garden', function (req, res) {
+		// replace old function with sequelize function
+		db.Plant.findAll()
+			// use promise method to pass the burgers...
+			.then(function (data) {
+				console.log(data);
+				// into the main index, updating the page
+				var hbsObject = { garden: data };
+				return res.render('index', hbsObject);
+			});
+	});
+};
 
 // module.exports = (app) => {
 // 	// get route that returns all plants
