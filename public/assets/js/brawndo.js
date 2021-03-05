d024eebcce6c566ce50379a88bc945ce0307c4ec
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-05 10:35:21 -0700

    work on js

--- public/assets/js/brawndo.js
+++ public/assets/js/brawndo.js
@@ -1,1 +1,32 @@
 console.log('Branwdo.js connected!');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+	console.log('DOM loaded!');
+
+	const createBurgerBtn = document.getElementById('create-form');
+
+	if (createBurgerBtn) {
+		createBurgerBtn.addEventListener('submit', (e) => {
+			e.preventDefault();
+			console.log('submitted!');
+
+			const newBurger = {
+				burger_name: document.getElementById('burger').value.trim(),
+				devoured: false,
+			};
+
+			fetch('/api/burgers', {
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+
+				body: JSON.stringify(newBurger),
+			}).then(() => {
+				document.getElementById('burger').value = '';
+				location.reload();
+			});
+		});
+	}
+});
