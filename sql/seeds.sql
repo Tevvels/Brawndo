a52c465866dc0b4996b8dda998f3abca790ef959
Dominique Horinka <76179868+domihorinka@users.noreply.github.com>
2021-03-08 10:52:51 -0700

    Update seeds.sql

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 INSERT INTO Plants (name, outdoorIndoor, waterFrequency, lastWatered)VALUES("ficus", false, 1, 2);
 INSERT INTO Plants (name, outdoorIndoor, waterFrequency, lastWatered)VALUES("strawberries", true, 1, 1);
-=======
-INSERT INTO plants (name, outdoorIndoor, waterFrequency, lastWatered)VALUES("ficus", false, 1, 2);
-INSERT INTO plants (name, outdoorIndoor, waterFrequency, lastWatered)VALUES("strawberries", true, 1, 1);
->>>>>>> main
+
