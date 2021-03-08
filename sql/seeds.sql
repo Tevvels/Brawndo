89d5b90b81fb0f75f6c8aff285cf79df42695b30
domihorinka <domihorinka@gmail.com>
2021-03-08 10:43:17 -0700

    added seeds file

---
+++ sql/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO plants (name, outdoorIndoor, waterFrequency, lastWatered)VALUES("ficus", false, 1, 2);
+INSERT INTO plants (name, outdoorIndoor, waterFrequency, lastWatered)VALUES("strawberries", true, 1, 1);
