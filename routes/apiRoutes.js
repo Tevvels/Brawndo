70b7706db6e01fa0c6cca15202f771bed3f54855
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-08 12:43:49 -0700

    FIXED BUTTONS '

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,37 +3,38 @@ const express = require('express');
 var router = express.Router();
 
 module.exports = (app) => {
-	app.get('/', function (req, res) {
-		// If plants exist in database, redirect to /garden. If no plants, redirect to /garden/create
-		db.Plant.findAll().then((data) => {
-			// * below functionality not currently working...need to debug
-			// console.log(data);
-			if (data) {
-				res.redirect('/garden');
-			} else {
-				res.redirect('/garden/add');
-			}
-		});
-	});
+	// app.get('/', function (req, res) {
+	// 	// If plants exist in database, redirect to /garden. If no plants, redirect to /garden/create
+	// 	db.Plant.findAll().then((data) => {
+	// 		// * below functionality not currently working...need to debug
+	// 		// console.log(data);
+	// 		if (data) {
+	// 			res.redirect('/garden');
+	// 		} else {
+	// 			res.redirect('/garden/add');
+	// 		}
+	// 	});
+	// });
 
 	// get route that displays all plants
 	app.get('/garden', function (req, res) {
-		console.log('get garden called');
 		db.Plant.findAll().then(function (data) {
-			console.log(data);
+			// console.log(data);
 			let hbsObject = { garden: data };
-			 res.render('index', hbsObject);
+			res.render('index', hbsObject);
 		});
+		console.log('get garden called');
 	});
 
 	// route to display 'add plant' page
 	app.get('/garden/add', function (req, res) {
-		res.render('addPlant');
+		console.log('"/garden/add" called');
+		return res.render('addPlant');
 	});
 
 	// post route that inserts a new plant into Plants in brawndo_db
 	app.post('/garden/add', (req, res) => {
-		console.log('hello')
-		db.Plant.create(req.body).then(/*res.redirect('/garden')*/);
+		console.log('hello');
+		db.Plant.create(req.body).then(res.redirect('/garden'));
 	});
 };
