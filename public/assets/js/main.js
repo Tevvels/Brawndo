bb42ec88397466154fb469ab9ded283e7c93c60c
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-08 18:19:12 -0700

    add homepage link

---
+++ public/assets/js/main.js
@@ -0,0 +1,11 @@
+const logo = document.getElementById('logo');
+
+logo.addEventListener('click', (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+	fetch('/garden', {
+		method: 'GET',
+	}).then((data) => {
+		window.location.replace('/garden');
+	});
+});
